69223c272d8d1b75d4790a40f6bdd6e1b20555ae
Owen <owan16@hotmail.com.tw>
2017-11-13 22:00:32 +0800

    Test

---
+++ the last person version/Mini-CEXentrustment/app/src/main/java/com/example/mini_cexentrustment/tw/MyFragmentAdapter.java
@@ -0,0 +1,93 @@
+package com.example.mini_cexentrustment.tw;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import com.example.mini_cexentrustment.R;
+import com.example.mini_cexentrustment.define.CommandType;
+import com.example.mini_cexentrustment.framework.BaseActivity;
+import com.example.mini_cexentrustment.thread.NetTask;
+import com.example.mini_cexentrustment.thread.OnThreadComplete;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+/**
+ * Created by Carson_Ho on 16/5/23.
+ */
+public class MyFragmentAdapter extends FragmentPagerAdapter
+
+    {
+
+        List<Fragment> list;
+
+
+
+        public MyFragmentAdapter(FragmentManager fm,List<Fragment> list) {
+        super(fm);
+        this.list=list;
+    }//写构造方法，方便赋值调用
+        @Override
+        public Fragment getItem(int arg0) {
+        return list.get(arg0);
+    }//根据Item的位置返回对应位置的Fragment，绑定item和Fragment
+
+        @Override
+        public int getCount() {
+        return list.size();
+    }//设置Item的数量
+
+    }
+
+
---
+++ the last person version/Mini-CEXentrustment/app/src/main/java/com/example/mini_cexentrustment/tw/TeachFragment.java
@@ -0,0 +1,24 @@
+package com.example.mini_cexentrustment.tw;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.example.mini_cexentrustment.R;
+
+/**
+ * Created by Carson_Ho on 16/5/23.
+ */
+public class TeachFragment extends Fragment
+
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_teach, null);
+        return view;
+    }
+}
+
